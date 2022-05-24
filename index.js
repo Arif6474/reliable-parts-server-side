e2dd3bf321d950d33e12c655d53916d17d7b82aa
Arif6474 <arifulislam64743@gmail.com>
2022-05-24 13:07:47 +0600

    create api for parts

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ app.get('/', (req, res) =>{
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tu9v0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function run() {
+  try {
+    await client.connect();
+    const partsCollection = client.db('reliable_parts').collection('parts');
+   
+    app.get("/part" , async (req, res) => {
+        const query ={}
+        const cursor = partsCollection.find(query);
+        const parts = await cursor.toArray();
+        res.send(parts);
+     })
+  } finally {
+   
+   
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 app.listen(port, () => {
     console.log('Reliable parts server is running ');
 })
